547e66e5a73024c1f5e1c3e16768485ec3055bab
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2025-02-02 20:24:50 -0600

    Added initial set up files

---
+++ interest-gauger/src/components/description/Description.jsx
@@ -0,0 +1,11 @@
+export const Description = () => {
+  return (
+    <div className="description-wrapper">
+      <h3>About the app</h3>
+      <p>
+        This is a simple application that allows you to search for GitHub users
+        and view their repositories.
+      </p>
+    </div>
+  );
+};
---
+++ interest-gauger/src/components/screenshots/Screenshots.jsx
@@ -0,0 +1,10 @@
+export const Screenshots = () => {
+  return (
+    <div className="screenshots-wrapper">
+      <h3>App Screenshots</h3>
+      <img src="/screenshots/screenshot1.png" alt="screenshot1" />
+      <img src="/screenshots/screenshot2.png" alt="screenshot2" />
+      <img src="/screenshots/screenshot3.png" alt="screenshot3" />
+    </div>
+  );
+}
